842bcb725ed1f7ac8e35e659a22d2ea2736a726c
Hong-She Liang <starofrainnight@gmail.com>
2017-07-17 20:02:31 +0800

    Fixed requirement name of "click"

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -7,9 +7,9 @@ with open('README.rst') as readme_file, open('HISTORY.rst') as history_file:
     long_description = (readme_file.read() + "\n\n" + history_file.read())
 
 requirements = [
-{%- if cookiecutter.command_line_interface|lower == 'click' %}
-    'Click>=6.0',
-{%- endif %}
+    {%- if cookiecutter.command_line_interface|lower == 'click' % }
+    'click>=6.0',
+    {%- endif % }
     # TODO: put package requirements here
 ]
 
@@ -17,9 +17,9 @@ test_requirements = [
     # TODO: put package test requirements here
 ]
 
-{%- if cookiecutter.license != "Not open source" %}
-    {%- set license = ('license' | pimport).find(cookiecutter.license) %}
-{%- endif %}
+{%- if cookiecutter.license != "Not open source" % }
+    {%- set license = ('license' | pimport).find(cookiecutter.license) % }
+{%- endif % }
 
 setup(
     name='{{ cookiecutter.project_slug }}',
@@ -34,26 +34,26 @@ setup(
     ],
     package_dir={'{{ cookiecutter.project_slug }}':
                  '{{ cookiecutter.project_slug }}'},
-{%- if 'no' not in cookiecutter.command_line_interface|lower %}
+    {%- if 'no' not in cookiecutter.command_line_interface|lower %}
     entry_points={
         'console_scripts': [
             '{{ cookiecutter.project_slug }}={{ cookiecutter.project_slug }}.__main__:main'
         ]
     },
-{%- endif %}
+    {%- endif %}
     include_package_data=True,
     install_requires=requirements,
-{%- if license is defined %}
+    {%- if license is defined %}
     license="{{ license.python.split(':')[-1].strip() }}",
-{%- endif %}
+    {%- endif %}
     zip_safe=False,
     keywords='{{ cookiecutter.project_slug }}',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
-{%- if license is defined %}
+        {%- if license is defined %}
         '{{ license.python }}',
-{%- endif %}
+        {%- endif %}
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
