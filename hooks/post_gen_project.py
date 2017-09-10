ecb9d3120029b9d8dab9ea7ce0da792f13d414ef
Hong-She Liang <starofrainnight@gmail.com>
2017-09-10 19:25:00 +0800

    Fixed test failed : test_bake_with_no_console_script()

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -14,7 +14,8 @@ if __name__ == '__main__':
         remove_file('docs/authors.rst')
 
     if 'no' in '{{ cookiecutter.command_line_interface|lower }}':
-        cli_file = os.path.join('{{ cookiecutter.project_slug }}', 'cli.py')
+        cli_file = os.path.join(
+            '{{ cookiecutter.project_slug }}', '__main__.py')
         remove_file(cli_file)
 
     if 'Not open source' == '{{ cookiecutter.license }}':
