1b7162c0b32eede2ceaac9ff20c2cf45b166adef
Hong-She Liang <starofrainnight@gmail.com>
2018-06-30 16:56:33 +0800

    Removed duplicated keywords if project_slug same with project_short_name

--- {{cookiecutter.project_short_name}}/setup.py
+++ {{cookiecutter.project_short_name}}/setup.py
@@ -56,7 +56,7 @@ setup(
     license="{{ license.python.split(':')[-1].strip() }}",
     {%- endif %}
     zip_safe=False,
-    keywords='{{ cookiecutter.project_slug }},{{ cookiecutter.project_short_name }}',
+    keywords='{{ [cookiecutter.project_slug, cookiecutter.project_short_name]|unique|join(",") }}',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
