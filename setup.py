73cd906d3a1da61f95ef055b53c4f69d18ce795a
Hong-She Liang <starofrainnight@gmail.com>
2019-01-26 13:45:35 +0800

    Bump version: 0.3.0 → 0.3.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(
     name='cookiecutter-pypackage',
     packages=[],
-    version='0.3.0',
+    version='0.3.1',
     description='Cookiecutter template for a Python package',
     author='Hong-She Liang',
     license='BSD',
